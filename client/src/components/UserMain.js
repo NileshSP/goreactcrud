a5b1deb356118029439257b2fac60c4312e8a165
Nilesh P <emailnileshp@gmail.com>
2019-02-16 18:41:36 -0500

    updated for port number

--- client/src/components/UserMain.js
+++ client/src/components/UserMain.js
@@ -6,7 +6,7 @@ class UserMain extends Component {
   constructor(props) {
     super(props)
     let inDev = process.env.NODE_ENV !== 'production';
-    let port = inDev ? 8080 : process.env.PORT;
+    let port = !inDev ? (process.env.PORT !== undefined ? process.env.PORT : 8081) : 8081;
     this.state = {
         users : new Set(),
         apiUrlBase : "http://localhost:"+ port,
